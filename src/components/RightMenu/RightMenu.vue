b25d625329aa6fd4eeecc857a89c380eddd48b1c
bmen25124 <bmen25124@gmail.com>
2025-11-12 21:04:27 +0300

    Added character item to right menu

--- src/components/RightMenu/RightMenu.vue
+++ src/components/RightMenu/RightMenu.vue
@@ -37,114 +37,96 @@ onMounted(() => {
       </div>
     </div>
   </div>
-  <div>
-    <div id="right-menu-panel" class="right-menu-panel">
-      <div class="right-menu-panel__header">
-        <div class="right-menu-panel__actions">
-          <div title="Create New Character" class="menu-button fa-solid fa-user-plus"></div>
-          <div title="Import Character from File" class="menu-button fa-solid fa-file-import"></div>
-          <div title="Import content from external URL" class="menu-button fa-solid fa-cloud-arrow-down"></div>
-          <div title="Create New Chat Group" class="menu-button fa-solid fa-users-gear"></div>
-          <div>
-            <!-- Container for additional buttons added by extensions -->
-          </div>
-          <select id="character-sort-order" class="text-pole" title="Characters sorting order">
-            <option value="search" hidden>Search</option>
-            <option value="name:asc">A-Z</option>
-            <option value="name:desc">Z-A</option>
-            <option value="create_date:desc">Newest</option>
-            <option value="create_date:asc">Oldest</option>
-            <option value="fav:desc">Favorites</option>
-            <option value="date_last_chat:desc">Recent</option>
-            <option value="chat_size:desc">Most chats</option>
-            <option value="chat_size:asc">Least chats</option>
-            <option value="data_size:desc">Most tokens</option>
-            <option value="data_size:asc">Least tokens</option>
-            <option value="random">Random</option>
-          </select>
-          <div
-            id="character-search-toggle"
-            class="menu-button-icon fa-fw fa-solid fa-search"
-            title="Toggle search bar"
-            @click="isSearchActive = !isSearchActive"
-          ></div>
+  <div id="right-menu-panel" class="right-menu-panel">
+    <div class="right-menu-panel__header">
+      <div class="right-menu-panel__actions">
+        <div title="Create New Character" class="menu-button fa-solid fa-user-plus"></div>
+        <div title="Import Character from File" class="menu-button fa-solid fa-file-import"></div>
+        <div title="Import content from external URL" class="menu-button fa-solid fa-cloud-arrow-down"></div>
+        <div title="Create New Chat Group" class="menu-button fa-solid fa-users-gear"></div>
+        <div>
+          <!-- Container for additional buttons added by extensions -->
         </div>
-        <div id="character-search-form" :class="{ active: isSearchActive }">
-          <input class="text-pole u-w-full" type="search" placeholder="Search..." />
-        </div>
-        <div class="tag-controls">
-          <div></div>
-          <div></div>
-        </div>
+        <select id="character-sort-order" class="text-pole" title="Characters sorting order">
+          <option value="search" hidden>Search</option>
+          <option value="name:asc">A-Z</option>
+          <option value="name:desc">Z-A</option>
+          <option value="create_date:desc">Newest</option>
+          <option value="create_date:asc">Oldest</option>
+          <option value="fav:desc">Favorites</option>
+          <option value="date_last_chat:desc">Recent</option>
+          <option value="chat_size:desc">Most chats</option>
+          <option value="chat_size:asc">Least chats</option>
+          <option value="data_size:desc">Most tokens</option>
+          <option value="data_size:asc">Least tokens</option>
+          <option value="random">Random</option>
+        </select>
+        <div
+          id="character-search-toggle"
+          class="menu-button-icon fa-fw fa-solid fa-search"
+          title="Toggle search bar"
+          @click="isSearchActive = !isSearchActive"
+        ></div>
       </div>
-
-      <div class="right-menu-panel__pagination">
-        <i class="fa-solid fa-table-cells-large menu-button" title="Toggle character grid view"></i>
-        <i
-          class="fa-solid fa-edit menu-button"
-          title="Bulk edit characters&#13;&#13;Click to toggle characters&#13;Shift + Click to select/deselect a range of characters&#13;Right-click for actions"
-        ></i>
+      <div id="character-search-form" :class="{ active: isSearchActive }">
+        <input class="text-pole u-w-full" type="search" placeholder="Search..." />
+      </div>
+      <div class="tag-controls">
         <div></div>
-        <i class="fa-solid fa-check-double menu-button" title="Bulk select all characters" style="display: none"></i>
-        <i class="fa-solid fa-trash menu-button" title="Bulk delete characters" style="display: none"></i>
+        <div></div>
       </div>
-      <div id="character-list" class="u-flex-col">
-        <div v-if="characterStore.displayableEntities.length === 0">Loading...</div>
-        <template v-for="entity in characterStore.displayableEntities" :key="entity.id">
-          <!-- Character Block -->
-          <div
-            v-if="entity.type === 'character'"
-            class="character-item"
-            :class="{ 'is-active': characterStore.activeCharacterIndex === entity.id }"
-            @click="characterStore.selectCharacterById(entity.id as number)"
-          >
-            <div class="character-item__avatar">
-              <img :src="getAvatarUrl((entity.item as Character).avatar)" alt="Character Avatar" />
-            </div>
-            <div class="character-item__name">{{ (entity.item as Character).name }}</div>
-          </div>
+    </div>
 
-          <!-- Group Block -->
-          <div v-if="entity.type === 'group'" class="character-item">
-            <!-- Group rendering logic here -->
-            <div class="character-item__name">GROUP: {{ (entity.item as Group).id }}</div>
+    <div class="right-menu-panel__pagination">
+      <i class="fa-solid fa-table-cells-large menu-button" title="Toggle character grid view"></i>
+      <i
+        class="fa-solid fa-edit menu-button"
+        title="Bulk edit characters&#13;&#13;Click to toggle characters&#13;Shift + Click to select/deselect a range of characters&#13;Right-click for actions"
+      ></i>
+      <div></div>
+      <i class="fa-solid fa-check-double menu-button" title="Bulk select all characters" style="display: none"></i>
+      <i class="fa-solid fa-trash menu-button" title="Bulk delete characters" style="display: none"></i>
+    </div>
+    <div id="character-list" class="u-flex-col">
+      <div v-if="characterStore.displayableEntities.length === 0">Loading...</div>
+      <template v-for="entity in characterStore.displayableEntities" :key="entity.id">
+        <!-- Character Block -->
+        <div
+          v-if="entity.type === 'character'"
+          class="character-item"
+          :class="{ 'is-active': characterStore.activeCharacterIndex === entity.id }"
+          @click="characterStore.selectCharacterById(entity.id as number)"
+          tabindex="0"
+          role="listitem"
+        >
+          <div class="character-item__avatar">
+            <img
+              :src="getAvatarUrl((entity.item as Character).avatar)"
+              :alt="`${(entity.item as Character).name} Avatar`"
+            />
           </div>
-        </template>
-      </div>
+          <div class="character-item__content">
+            <div class="character-item__header">
+              <span class="character-item__name" :title="(entity.item as Character).name">{{
+                (entity.item as Character).name
+              }}</span>
+              <i class="character-item__fav-icon fa-solid fa-star" v-if="(entity.item as Character).fav"></i>
+            </div>
+            <div class="character-item__description">
+              {{ (entity.item as Character).description }}
+            </div>
+            <div class="character-item__tags" v-if="(entity.item as Character).tags?.length">
+              <span v-for="tag in (entity.item as Character).tags" :key="tag" class="tag">{{ tag }}</span>
+            </div>
+          </div>
+        </div>
+
+        <!-- Group Block -->
+        <div v-if="entity.type === 'group'" class="character-item">
+          <!-- Group rendering logic here -->
+          <div class="character-item__name">GROUP: {{ (entity.item as Group).id }}</div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-/* Basic styles to replicate the character list appearance */
-.character-item {
-  display: flex;
-  align-items: center;
-  padding: 5px;
-  cursor: pointer;
-  border-radius: 5px;
-  margin: 2px 0;
-
-  &:hover {
-    background-color: var(--white-20a);
-  }
-
-  &.is-active {
-    background-color: var(--color-active);
-  }
-}
-
-.character-item__avatar img {
-  width: 40px;
-  height: 40px;
-  border-radius: 5px;
-  margin-right: 10px;
-  object-fit: cover;
-}
-
-.character-item__name {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,10 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
+      '/thumbnail': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
     },
   },
   build: {
